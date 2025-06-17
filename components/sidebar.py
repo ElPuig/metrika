800cf3a871618b03fa324949c5130ae7046a7311
Darío Pérez <daro.nahuel@gmail.com>
2025-06-17 17:29:15 +0200

    feat: remove components

--- components/sidebar.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def create_sidebar():
-    with st.sidebar:
-        st.title("Navigation")
-        page = st.radio("Go to", ["Home", "Analysis", "Settings"])
-    return page
